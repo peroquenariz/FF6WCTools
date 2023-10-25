f8e31e9b33c48e7f0ed27f46e555c52454c10f73
peroquenariz <el.ace100@gmail.com>
2023-10-24 23:36:47 -0300

    Added base abstract class for game data

---
+++ FF6WCToolsLib/DataTemplates/BaseData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static FF6WCToolsLib.WCData;
+
+namespace FF6WCToolsLib.DataTemplates;
+
+public abstract class BaseData
+{
+    public abstract uint DataStartAddress { get; }
+    public abstract byte DataBlockSize { get; }
+    public abstract int DataBlockCount { get; }
+
+    public abstract byte[] ToByteArray();
+
+    public new abstract string ToString();
+}

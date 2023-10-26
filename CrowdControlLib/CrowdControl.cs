bec2262b76c29d25862aff6024064d6661d761d8
peroquenariz <el.ace100@gmail.com>
2023-10-26 11:14:55 -0300

    Added preliminary data initialization for testing

--- CrowdControlLib/CrowdControl.cs
+++ CrowdControlLib/CrowdControl.cs
@@ -1,9 +1,11 @@
 ﻿using FF6WCToolsLib;
+using static FF6WCToolsLib.WCData;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using Message = TwitchChatbot.Message;
+using FF6WCToolsLib.DataTemplates;
 
 namespace CrowdControlLib;
 
@@ -13,19 +15,66 @@ public class CrowdControl
     private readonly string? _libVersion;
 
     private List<Message> _crowdControlMessageQueue;
+
+    private readonly byte[] _defaultSpellData;
+    private readonly byte[] _defaultItemData;
+
+    private readonly List<SpellData> _spellDataList;
+    private readonly List<ItemData> _itemDataList;
     
+
     public string? LibVersion { get => _libVersion; }
 
     public CrowdControl(SniClient sniClient, List<Message> crowdControlMessageQueue)
     {
         _libVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
         _sniClient = sniClient;
-
         _crowdControlMessageQueue = crowdControlMessageQueue;
+
+        _spellDataList = new List<SpellData>();
+        _itemDataList = new List<ItemData>();
+
+        // Get all ROM default data.
+        _defaultSpellData = _sniClient.ReadMemory(SPELL_DATA_START, SPELL_DATA_BLOCK_COUNT * SPELL_DATA_BLOCK_SIZE);
+        _defaultItemData = _sniClient.ReadMemory(ITEM_DATA_START, ITEM_DATA_BLOCK_COUNT * ITEM_DATA_BLOCK_SIZE);
+        
+        // Create data structures.
+        InitializeData();
     }
 
+    /// <summary>
+    /// Populates data lists with current loaded ROM data.
+    /// </summary>
+    private void InitializeData()
+    {
+        for (int i = 0; i < SPELL_DATA_BLOCK_COUNT; i++)
+        {
+            int spellStart = i * SPELL_DATA_BLOCK_SIZE;
+            int spellEnd = (i+1) * SPELL_DATA_BLOCK_SIZE;
+            _spellDataList.Add(new SpellData(_defaultSpellData[spellStart..spellEnd], i));
+        }
+
+        for (int i = 0; i < ITEM_DATA_BLOCK_COUNT; i++)
+        {
+            int itemStart = i * ITEM_DATA_BLOCK_SIZE;
+            int itemEnd = (i + 1) * ITEM_DATA_BLOCK_SIZE;
+            _itemDataList.Add(new ItemData(_defaultItemData[itemStart..itemEnd], i));
+        }
+    }
+    
     public async Task ExecuteAsync()
     {
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            await Console.Out.WriteLineAsync(e.ToString());
+        }
+
+        //await Console.Out.WriteLineAsync(zeroCount.ToString());
+
         while (true)
         {
             await Task.Delay(5000);

ef939a6890dcede2ea1e4ff3686282cef58a4256
peroquenariz <el.ace100@gmail.com>
2023-10-26 11:14:13 -0300

    Refactored data structure, all common data and logic is part of the base class now

--- FF6WCToolsLib/DataTemplates/BaseData.cs
+++ FF6WCToolsLib/DataTemplates/BaseData.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using static FF6WCToolsLib.WCData;
-
-namespace FF6WCToolsLib.DataTemplates;
+﻿namespace FF6WCToolsLib.DataTemplates;
 
 public abstract class BaseData
 {
-    public abstract uint DataStartAddress { get; }
-    public abstract byte DataBlockSize { get; }
-    public abstract int DataBlockCount { get; }
+    protected readonly byte[] _defaultData;
+    protected byte[] _data;
+    protected int _dataIndex;
+    
+    public abstract uint StartAddress { get; }
+    public abstract byte BlockSize { get; }
+    public abstract int BlockCount { get; }
+    public uint TargetAddress => StartAddress + (uint)(BlockSize * _dataIndex);
 
-    public abstract byte[] ToByteArray();
-
+    protected BaseData(byte[] data, int dataIndex)
+    {
+        _dataIndex = dataIndex;
+        _defaultData = data;
+        _data = (byte[])_defaultData.Clone();
+    }
+    
     public new abstract string ToString();
+
+    public byte[] ToByteArray()
+    {
+        return _data;
+    }
+
+    public void ResetData()
+    {
+        _data = (byte[])_defaultData.Clone();
+    }
 }
--- FF6WCToolsLib/DataTemplates/SpellData.cs
+++ FF6WCToolsLib/DataTemplates/SpellData.cs
@@ -6,42 +6,33 @@ namespace FF6WCToolsLib.DataTemplates;
 
 public class SpellData : BaseData
 {
-    private readonly int _spellIndex;
-    private byte[] _spellData;
+    public override uint StartAddress => SPELL_DATA_START;
+    public override byte BlockSize => SPELL_DATA_BLOCK_SIZE;
+    public override int BlockCount => SPELL_DATA_BLOCK_COUNT;
 
-    public override uint DataStartAddress => SPELL_DATA_START;
-    public override byte DataBlockSize => SPELL_DATA_BLOCK_SIZE;
-    public override int DataBlockCount => SPELL_DATA_BLOCK_COUNT;
-    
-    public SpellData(byte[] data, int spellIndex)
+    public SpellData(byte[] defaultSpellData, int spellIndex) : base(defaultSpellData, spellIndex)
     {
-        _spellIndex = spellIndex;
-        _spellData = data;
-    }
-
-    public override byte[] ToByteArray()
-    {
-        return _spellData;
+        
     }
 
     public override string ToString()
     {
         string spellDescription =
-            $"Spell name: {SPELL_DICT[(byte)_spellIndex]}\n" + // TODO: replace with current name in memory!
-            $"Targeting: {(Targeting)_spellData[(int)SpellDataStructure.Targeting]}\n" +
-            $"Elemental properties: {(ElementalProperties)_spellData[(int)SpellDataStructure.ElementalProperties]}\n" +
-            $"Spell flags 1: {(SpellFlags1)_spellData[(int)SpellDataStructure.SpellFlags1]}\n" +
-            $"Spell flags 2: {(SpellFlags2)_spellData[(int)SpellDataStructure.SpellFlags2]}\n" +
-            $"Spell flags 3: {(SpellFlags3)_spellData[(int)SpellDataStructure.SpellFlags3]}\n" +
-            $"MP cost: {_spellData[(int)SpellDataStructure.MPCost]}\n" +
-            $"Spell power: {_spellData[(int)SpellDataStructure.SpellPower]}\n" +
-            $"Spell flags 4: {(SpellFlags4)_spellData[(int)SpellDataStructure.SpellFlags4]}\n" +
-            $"Hit rate: {_spellData[(int)SpellDataStructure.HitRate]}\n" +
-            $"Special effect: {_spellData[(int)SpellDataStructure.SpecialEffect]}\n" +
-            $"Status 1: {(StatusCondition1)_spellData[(int)SpellDataStructure.Status1]}\n" +
-            $"Status 2: {(StatusCondition2)_spellData[(int)SpellDataStructure.Status2]}\n" +
-            $"Status 3: {(StatusCondition3)_spellData[(int)SpellDataStructure.Status3]}\n" +
-            $"Status 4: {(StatusCondition4)_spellData[(int)SpellDataStructure.Status4]}\n";
+            $"Spell name: {SPELL_DICT[(byte)_dataIndex]}\n" + // TODO: replace with current name in memory!
+            $"Targeting: {(Targeting)_data[(int)SpellDataStructure.Targeting]}\n" +
+            $"Elemental properties: {(ElementalProperties)_data[(int)SpellDataStructure.ElementalProperties]}\n" +
+            $"Spell flags 1: {(SpellFlags1)_data[(int)SpellDataStructure.SpellFlags1]}\n" +
+            $"Spell flags 2: {(SpellFlags2)_data[(int)SpellDataStructure.SpellFlags2]}\n" +
+            $"Spell flags 3: {(SpellFlags3)_data[(int)SpellDataStructure.SpellFlags3]}\n" +
+            $"MP cost: {_data[(int)SpellDataStructure.MPCost]}\n" +
+            $"Spell power: {_data[(int)SpellDataStructure.SpellPower]}\n" +
+            $"Spell flags 4: {(SpellFlags4)_data[(int)SpellDataStructure.SpellFlags4]}\n" +
+            $"Hit rate: {_data[(int)SpellDataStructure.HitRate]}\n" +
+            $"Special effect: {_data[(int)SpellDataStructure.SpecialEffect]}\n" +
+            $"Status 1: {(StatusCondition1)_data[(int)SpellDataStructure.Status1]}\n" +
+            $"Status 2: {(StatusCondition2)_data[(int)SpellDataStructure.Status2]}\n" +
+            $"Status 3: {(StatusCondition3)_data[(int)SpellDataStructure.Status3]}\n" +
+            $"Status 4: {(StatusCondition4)_data[(int)SpellDataStructure.Status4]}\n";
         return spellDescription;
     }
 }
